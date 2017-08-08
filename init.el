6387b0a16b2f65fb6d689fa965e6a6ee0e99e7bd
gao enbo <gao@gaodeMacBook-Pro.local>
2017-08-08 10:24:06 +0800

    mouse super emacs 24.5

--- init.el
+++ init.el
@@ -140,13 +140,20 @@
 
 
 
-; mouse click superd
 
-;(require 'mouse)
-(xterm-mouse-mode t)
-;(defun track-mouse (e)) 
-;(setq mouse-sel-mode t)
-
+;; Enable mouse support  emacs version 24.5.1
+(unless window-system
+  (require 'mouse)
+  (xterm-mouse-mode t)
+  (global-set-key [mouse-4] (lambda ()
+			      (interactive)
+			      (scroll-down 1)))
+  (global-set-key [mouse-5] (lambda ()
+			      (interactive)
+			      (scroll-up 1)))
+  (defun track-mouse (e))
+  (setq mouse-sel-mode t)
+  )
 
 ; auto save 
 
